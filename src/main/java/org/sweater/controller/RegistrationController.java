6c41dc34139dd311f6c90c54ef6cf5130620db3a
Viktor <54067422+ChepelVE@users.noreply.github.com>
2022-04-24 20:02:00 +0300

    user editor and admin role added

--- src/main/java/org/sweater/controller/RegistrationController.java
+++ src/main/java/org/sweater/controller/RegistrationController.java
@@ -2,6 +2,7 @@ package org.sweater.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.sweater.domain.Role;
@@ -18,15 +19,16 @@ public class RegistrationController {
 
     @GetMapping("/registration")
     public String registration(){
+
         return "registration";
     }
 
     @PostMapping("/registration")
-    public String addUser(User user, Map<String, Object> model) {
+    public String addUser(User user, Model model) {
         User userFromDB = userRepo.findByUsername(user.getUsername());
 
         if(userFromDB != null) {
-            model.put("message", "User exists!");
+            model.addAttribute("message", "User exists!");
             return "registration";
         }
 
