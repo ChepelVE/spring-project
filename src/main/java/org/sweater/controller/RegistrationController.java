0eed2ef6afd6cc8dbcb37535bc66517f35a202e8
ViktorChepelTR <63342270+ViktorChepelTR@users.noreply.github.com>
2022-04-27 14:43:27 +0300

    account activation via email

--- src/main/java/org/sweater/controller/RegistrationController.java
+++ src/main/java/org/sweater/controller/RegistrationController.java
@@ -4,18 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.sweater.domain.Role;
 import org.sweater.domain.User;
-import org.sweater.repos.UserRepo;
-
-import java.util.Collections;
-import java.util.Map;
+import org.sweater.service.UserService;
 
 @Controller
 public class RegistrationController {
     @Autowired
-    private UserRepo userRepo;
+    private UserService userService;
 
     @GetMapping("/registration")
     public String registration(){
@@ -25,17 +22,24 @@ public class RegistrationController {
 
     @PostMapping("/registration")
     public String addUser(User user, Model model) {
-        User userFromDB = userRepo.findByUsername(user.getUsername());
 
-        if(userFromDB != null) {
+        if(!userService.addUser(user)) {
             model.addAttribute("message", "User exists!");
             return "registration";
         }
 
-        user.setActive(true);
-        user.setRoles(Collections.singleton(Role.USER));
-        userRepo.save(user);
-
         return "redirect:/login";
+    }
+
+    @GetMapping("/activate/{code}")
+    public String activate(Model model, @PathVariable String code) {
+        boolean isActivated = userService.activateUser(code);
+
+        if (isActivated) {
+            model.addAttribute("message", "User successfully activated!");
+        } else {
+            model.addAttribute("message", "Activation code is not found!");
+        }
+        return "login";
     }
 }
