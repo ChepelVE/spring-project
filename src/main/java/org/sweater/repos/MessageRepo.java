abee5247b268cf840c7e8ce3fbacf090a779dd7b
ViktorChepelTR <63342270+ViktorChepelTR@users.noreply.github.com>
2022-05-20 19:59:54 +0300

    pagination

--- src/main/java/org/sweater/repos/MessageRepo.java
+++ src/main/java/org/sweater/repos/MessageRepo.java
@@ -1,10 +1,12 @@
 package org.sweater.repos;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.sweater.domain.Message;
 
-import java.util.List;
-
 public interface MessageRepo extends CrudRepository<Message, Long> {
-    List<Message> findByTag(String tag);
+    Page<Message> findByTag(String tag, Pageable pageable);
+
+    Page<Message> findAll(Pageable pageable);
 }
